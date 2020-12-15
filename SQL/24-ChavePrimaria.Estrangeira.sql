448655a3a60f8d2ca716bb33428dfdb46b004bbe
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-15 20:26:09 -0300

    24

---
+++ SQL/24-ChavePrimaria.Estrangeira.sql
@@ -0,0 +1,16 @@
+-- Chave Primaria e Estrangeiras  ->é uma coluna ou grupo de colunas, usada para identificar unicamente uma linha em uma tabela
+
+/*
+CREATE TABLE nome_Tabela (
+	nomeColuna tipoDeDados PRIMARY KEY
+	nomeCOLUNA tipoDeDados ...
+)
+*/
+
+/*
+## Chave estrangeira
+	É uma coluna ou grupo de colunas que é uma chave primaria em outra tabela
+	A tabela que contem a chave estrangeira é chamada de tabela referencia ou tabela filho
+	E a tabela na qual é referenciada é chamada de tabela referencial ou tabela pai
+	Uma tabela pode ter mais de uma chave estrangeira
+*/
